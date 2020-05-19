21b7ca3ab55d97cde3277bf1ab8e96f0d6388feb
Kenneth Tenny <kenneth.tenny@maximl.com>
2020-05-20 03:25:29 +0530

    Added Backend + Estabilished connection with MongoDB with help of Mongoose

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+
+// const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.ATLAS_URI;
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true } );
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("DB connected!");
+});
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
